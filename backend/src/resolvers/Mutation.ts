cc49c5476a75cb429237f7bccf043b67cba165d6
Art Longbottom <art.longbottom.jr@gmail.com>
2018-11-20 03:42:07 -0800

    start scaffolding schema

--- backend/src/resolvers/Mutation.ts
+++ backend/src/resolvers/Mutation.ts
@@ -1,1 +1,11 @@
-export const Mutation = {}
+// This resolver file was scaffolded by github.com/prisma/graphqlgen, DO NOT EDIT.
+// Please do not import this file directly but copy & paste to your application code.
+
+import { MutationResolvers } from '../generated/graphqlgen'
+
+export const Mutation: MutationResolvers.Type = {
+  ...MutationResolvers.defaultResolvers,
+  createUser: (parent, args, ctx) => {
+    throw new Error('Resolver not implemented')
+  }
+}
